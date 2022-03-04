043c09b6d122d620a65b151fa091999c38919200
zzhang025 <zhangzx45@gmail.com>
2022-03-04 13:40:09 -0800

    App test

--- js/site.js
+++ js/site.js
@@ -1,4 +1,53 @@
-//Call hello world
-function Helloworld(){
-    alert("hello world");
+// Javascript
+
+// Get input Values
+// Controller Function
+
+function getValues(){
+    // Obtain values from the page
+    let fizz = document.getElementById("startValue").value;
+    let buzz = document.getElementById("endValue").value;
+
+    // parse the values into integer
+    fizz = parseInt(fizz);
+    buzz = parseInt(buzz);
+
+    //Validate Inputs
+    if (Number.isInteger(fizz) && Number.isInteger(buzz)) {
+        let numbers= buzzNumbers(fizz,buzz)
+    } else {
+        alert("Inputs must be integers")
+    }
+
+    // Generate Fizzbuzz
+    let numbers = buzzNumbers(fizz,buzz);
+
+    // Display the Numbers
+    displayNumbers(numbers);
+}
+
+// Generate numbers
+function buzzNumbers(fizz,buzz){
+    let numbers = Array.from(Array(100).keys());
+    return numbers;
+}
+
+// Display and Replace
+function displayNumbers(numbers){
+    let templateRows ="";
+    let className="even";
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+        if(number % 2 == 0){
+            className="even";
+        } else{
+            className="odd";
+        }
+
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows
+
+
 }
