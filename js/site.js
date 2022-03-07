2b88d2089247cf69580ab445eec38ce0f4b78312
zzhang025 <zhangzx45@gmail.com>
2022-03-06 17:59:14 -0800

    Site.js update

--- js/site.js
+++ js/site.js
@@ -2,7 +2,6 @@
 
 // Get input Values
 // Controller Function
-
 function getValues(){
     // Obtain values from the page
     let fizz = document.getElementById("startValue").value;
@@ -12,46 +11,67 @@ function getValues(){
     fizz = parseInt(fizz);
     buzz = parseInt(buzz);
 
-    // Generate Fizzbuzz
-    let numbers = generateNumbers();
-
-    // Display the Numbers
-    displayNumbers(numbers);
-}
-
-// Generate numbers
-function generateNumbers(){
-    let numbers = Array.from(Array(100).keys());
-    
-    return numbers;
-}
-
-// Display and Replace
-function displayNumbers(numbers){
-// Obtain values from the page
-    let fizz = document.getElementById("startValue").value;
-    let buzz = document.getElementById("endValue").value;
-
-    let templateRows ="";
-    for (let index = 1; index < numbers.length; index++) {
-        if(index % fizz ==0 && index % buzz==0){
-            color = "text-red";
-            number = "fizzbuzz";
-        } else if(index % fizz ==0 ){
-            color = "text-green";
-            number = "fizz";
-        } else if(index % buzz ==0 ) {
-            color = "text-blue";
-            number ="buzz";
-        } else {
-            color = ""
-            number = index;
-        }
-        
-        templateRows += `<tr><td class="${color}">${number}</td></tr>`;
+    // check the numbers if they are integer 
+    if (Number.isInteger(fizz) && Number.isInteger(buzz)) {
+        // call fizzbuzz//
+        let fbArray = FizzBuzz(fizz,buzz);
+        displayData(fbArray);
+    } else {
+        alert("You must Enter Integers")
     }
-    
-    document.getElementById("results").innerHTML = templateRows;
+}
 
+// do fizzbuzz
+function fizzbuzz(fizz,buzz){
+
+    let returnArray = [];
+
+    //loop from 1 to 100
+    for (let i=1; i <= 100; i++){
+        // check if fizzbuzz first, then fizz, then buzz
+        if(i % fizz ==0 && i % buzz==0){
+            color = "text-red";
+            returnArray.push('FizzBuzz');
+        } else if(i % fizz ==0 ){
+            color = "text-green";
+            returnArray.push('Fizz');
+        } else if(i % buzz ==0 ) {
+            color = "text-blue";
+            returnArray.push('Buzz');
+        } else {
+            color = ""; 
+            returnArray.push(i);
+        }
+    }
+    return returnArray;
+}
+
+// Display Data
+function displayData(fbArray){
+
+    //Get the table body element from the page
+    let tablebody = document.getElementById("results");
+
+    //Get the templat row
+    let templateRow = document.getElementById("fbTemplate");
+
+    //Clear table first
+    tablebody.interHTML="";
+
+    for (let index = 0; index < fbArray.length; index += 5) {
+
+        let tableRow = document.importNode(templateRow.content,true);
+        
+        //Grab the td put into array
+        let rowCols = tableRow.querySelectorAll("td");
+        rowCols[0].textContent = fbArray[index]; 
+        rowCols[1].textContent = fbArray[index+1];
+        rowCols[2].textContent = fbArray[index+2];
+        rowCols[3].textContent = fbArray[index+3];
+        rowCols[4].textContent = fbArray[index+4];
+        
+        tablebody.appendChild(tableRow);
+    }
+    //Add all the rows to the table
 
 }
